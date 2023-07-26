92ac28e743e529eab9d31a0b0b63a030a81f5dda
thunha14 <painkiller.nhat@gmail.com>
2023-07-26 15:45:39 +0700

    update unit_test and upgrade_process

--- data/unit_test.py
+++ data/unit_test.py
@@ -7,29 +7,25 @@ class CodeGenerator:
         # self.dest = "test_utils.sh"
         self.bin_path = "/opt/odoo/odoo-bin"
         self.conf_path = "/etc/odoo.conf"
-        self.mod_path = "modules.txt"
         self.rootDir = "./unit_test"
-        # self.index = 0
+        self.mod_list = []
     
     def check_path(self, fpath):
         return os.path.isfile(fpath)
     
     def run(self):
-        # f = open(self.dest, "w")
         code = self.genCode()
         print(code)
+        self.mod_list = []
     
-    def check_module(self, fpath):
-        with open(fpath, "w") as f:
-            for entry in os.scandir(self.rootDir):
-                if entry.is_dir():
-                    f.write(entry.name + '\n')
-        
-        with open(fpath, "r") as f:
-            return [modules.strip() for modules in f]
+    def check_module(self):
+        for entry in os.scandir(self.rootDir):
+            if entry.is_dir():
+                self.mod_list.append(entry.name)
+        return self.mod_list
 
     def genCode(self):
-        modules_list = self.check_module(self.mod_path)
+        modules_list = self.check_module()
         code = "#!/bin/bash \n \n" + self.bin_path + " -c " + self.conf_path
         # Load module
         # First argument from input: database name
---
+++ data/upgrade_process.py
@@ -0,0 +1,70 @@
+import yaml
+from functools import reduce
+import subprocess
+import os
+
+list_addons = ["customized_addons"]
+
+class GetChange:
+    def __init__(self):
+        self.commit_stat = ""
+        try:
+            self.commit_stat = subprocess.check_output(['git', 'show', '--stat', 'HEAD'], text=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Error occurred: {e}")
+
+    def list_subdirectories(self, addons):
+        directory = os.getcwd() + '/' + addons
+        target_file = '__manifest__.py'
+        subdirectories = set()
+        for dir_path, dir_names, filenames in os.walk(directory):
+            if target_file in filenames:
+                subdirectories.add(os.path.basename(dir_path))
+        return subdirectories
+
+    
+    def run(self):
+        git_change_set = set()
+        for addons in list_addons:
+            module_list = self.list_subdirectories(addons)
+            for module in module_list:
+                if self.commit_stat.find(module) != -1:
+                    git_change_set.add(module)
+        return git_change_set
+
+class CodeGenerator:
+    def __init__(self):
+        self.up_list = set()
+        self.path = "upgrade_module.yaml"
+    
+    def run(self):
+        self.genCode()
+        return self.up_list
+        
+    def genCode(self):
+        _, instruc = self.read_yaml_file()
+        if instruc['upgrade_modules'] is not None:
+            for x in instruc['upgrade_modules']:
+                self.up_list.add(x)
+
+    def read_yaml_file(self):
+        with open(self.path, 'r') as file:
+            data = yaml.safe_load(file)
+        ver = list(data.keys())[0]
+        instruc = list(data.values())[0]
+        return ver, instruc
+    
+if __name__ == "__main__":
+    code = CodeGenerator()
+    uplist = code.run()
+    git_change = GetChange()
+    change_list = git_change.run()
+    missing = change_list - uplist
+    missing = ('A', 'B')
+    if len(missing) > 0:
+        result_list = [str(item) for item in missing]
+        result_string = ",".join(result_list)
+    else:
+        result_string = ""
+    print(result_string)
+    print(",".join(uplist))
